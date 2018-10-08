b65ce73a9471b9873a2b7bdb8a00580a3f12e6a1
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-10-09 00:12:27 +0900

    土台の回転部分のArduino Uno

---
+++ Hammer_Throw/Arduino/ArduinoUno_DCMotor/ArduinoUno_DCMotor.ino
@@ -0,0 +1,58 @@
+//sound
+int sound_sensor = A0;
+int sound_value;
+
+//DCMotor
+int motor = 11;
+
+//Switch
+int button = 2;
+//LED
+int led = 13;
+//状態分け LED付いてたらON
+int val = 0;
+int old_val = 0;
+int state = 0;
+
+void setup() {
+  //DCmotor
+  pinMode(motor, OUTPUT);
+  
+  //Sound
+  pinMode(sound_sensor, INPUT);
+  Serial.begin(9600);
+  
+  //Switch
+  pinMode(button, INPUT);
+  //LED
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  //Sound
+  sound_value = analogRead(sound_sensor);
+  Serial.println(sound_value);
+  
+  //DCMotor
+  //LEDついてたら動く
+  if (state == 1) {
+    if (sound_value > 50) {
+    digitalWrite(motor, HIGH);
+    } else {
+    digitalWrite(motor, LOW);
+    }
+  }
+  
+  //Switch 状態分け LED付いてたら動く
+  val = digitalRead(button);
+  if ((val == HIGH) && (old_val == LOW)) {
+    state = 1 - state;
+    delay(10);
+  }
+  old_val = val;
+  if (state == 1) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
