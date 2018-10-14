fa449cc8c20d9fe00e76ae5fb72754c97385d7ab
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-10-14 15:53:43 +0200

    fix

--- Archery/Arduino/Serial2Knob0/Serial2Knob0.ino
+++ Archery/Arduino/Serial2Knob0/Serial2Knob0.ino
@@ -17,7 +17,7 @@ int potpin = 0;  // analog pin used to connect the potentiometer
 int val = 's';    // variable to read the value from the analog pin
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
@@ -25,79 +25,79 @@ void loop() {
   val = Serial.read();// reads the value of the potentiometer (value between 0 and 1023)
   //Serial.print(val);
   if(val == 'a'){
-    myservo.write(5,70,true);
+    myservo.write(175,70,true);
   }else  if(val == 'b'){
-    myservo.write(10,70,true);
+    myservo.write(170,70,true);
   }else  if(val == 'c'){
-    myservo.write(15,70,true);
+    myservo.write(165,70,true);
   }else  if(val == 'd'){
-    myservo.write(20,70,true);
+    myservo.write(160,70,true);
   }else  if(val == 'e'){
-    myservo.write(25,70,true);
+    myservo.write(155,70,true);
   }else  if(val == 'f'){
-    myservo.write(30,70,true);
+    myservo.write(150,70,true);
   }else  if(val == 'g'){
-    myservo.write(35,70,true);
+    myservo.write(145,70,true);
   }else  if(val == 'h'){
-    myservo.write(40,70,true);
+    myservo.write(140,70,true);
   }else  if(val == 'i'){
-    myservo.write(45,70,true);
+    myservo.write(135,70,true);
   }else  if(val == 'j'){
-    myservo.write(50,70,true);
+    myservo.write(130,70,true);
   }else  if(val == 'k'){
-    myservo.write(55,70,true);
+    myservo.write(125,70,true);
   }else  if(val == 'l'){
-    myservo.write(60,70,true);
+    myservo.write(120,70,true);
   }else  if(val == 'm'){
-    myservo.write(65,70,true);
+    myservo.write(115,70,true);
   }else  if(val == 'n'){
-    myservo.write(70,70,true);
+    myservo.write(110,70,true);
   }else  if(val == 'o'){
-    myservo.write(75,70,true);
+    myservo.write(105,70,true);
   }else  if(val == 'p'){
-    myservo.write(80,70,true);
+    myservo.write(100,70,true);
   }else  if(val == 'q'){
-    myservo.write(85,70,true);
+    myservo.write(95,70,true);
   }else  if(val == 'r'){
     myservo.write(90,70,true);
   }else  if(val == 's'){
-    myservo.write(95,70,true);
+    myservo.write(85,70,true);
   }else  if(val == 't'){
-    myservo.write(100,70,true);
+    myservo.write(80,70,true);
   }else  if(val == 'u'){
-    myservo.write(105,70,true);
+    myservo.write(75,70,true);
   }else  if(val == 'v'){
-    myservo.write(110,70,true);
+    myservo.write(70,70,true);
   }else  if(val == 'w'){
-    myservo.write(115,70,true);
+    myservo.write(65,70,true);
   }else  if(val == 'x'){
-    myservo.write(120,70,true);
+    myservo.write(60,70,true);
   }else  if(val == 'y'){
-    myservo.write(125,70,true);
+    myservo.write(55,70,true);
   }else  if(val == 'z'){
-    myservo.write(130,70,true);
+    myservo.write(50,70,true);
   }else  if(val == '0'){
-    myservo.write(135,70,true);
+    myservo.write(45,70,true);
   }else  if(val == '1'){
-    myservo.write(140,70,true);
+    myservo.write(40,70,true);
   }else  if(val == '2'){
-    myservo.write(145,70,true);
+    myservo.write(35,70,true);
   }else  if(val == '3'){
-    myservo.write(150,70,true);
+    myservo.write(30,70,true);
   }else  if(val == '4'){
-    myservo.write(155,70,true);
+    myservo.write(25,70,true);
   }else  if(val == '5'){
-    myservo.write(160,70,true);
+    myservo.write(20,70,true);
   }else  if(val == '6'){
-    myservo.write(165,70,true);
+    myservo.write(15,70,true);
   }else  if(val == '7'){
-    myservo.write(170,70,true);
+    myservo.write(10,70,true);
   }else  if(val == '8'){
-    myservo.write(175,70,true);
+    myservo.write(5,70,true);
   }else  if(val == '9'){
-    myservo.write(180,70,true);
+    myservo.write(0,70,true);
   }
   //val = map(val, 0, 180, 0, 180);// scale it to use it with the servo (value between 0 and 180)
   //myservo.write(180); // sets the servo position according to the scaled value
-  delay(15);//ここの値を100~300くらいのあたいで試行錯誤すれば動く
+  delay(100);//ここの値を100~300くらいのあたいで試行錯誤すれば動く
 }//ターミナルでdoa.pyを実行した後に、ボードへの書き込みを繰り返すと動き出す
--- Archery/Python/usb_4_mic_array/DOA.py
+++ Archery/Python/usb_4_mic_array/DOA.py
@@ -4,88 +4,90 @@ import usb.util
 import time
 import serial
 
-
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-ser = serial.Serial("/dev/cu.usbmodem14131", 115200, timeout=None)
+ser = serial.Serial("/dev/cu.usbmodem14221", 9600, timeout=None)
 if ser:
     Mic_tuning = Tuning(dev)
     while True:
-        print Mic_tuning.direction
-        if(0 <= Mic_tuning.direction <= 5):
-            flag = 'a'
-        if(5 < Mic_tuning.direction <= 10):
-            flag = 'b';
-        if(10 < Mic_tuning.direction <= 15):
-            flag = 'c';
-        if(15 < Mic_tuning.direction <= 20):
-            flag = 'd';
-        if(20 < Mic_tuning.direction <= 25):
-            flag = 'e';
-        if(25 < Mic_tuning.direction <= 30):
-            flag = 'f';
-        if(30 < Mic_tuning.direction <= 35):
-            flag = 'g';
-        if(35 < Mic_tuning.direction <= 40):
-            flag = 'h';
-        if(40 < Mic_tuning.direction <= 45):
-            flag = 'i';
-        if(45 < Mic_tuning.direction <= 50):
-            flag = 'j';
-        if(50 < Mic_tuning.direction <= 55):
-            flag = 'k';
-        if(55 < Mic_tuning.direction <= 60):
-            flag = 'l';
-        if(60 < Mic_tuning.direction <= 65):
-            flag = 'm';
-        if(65 < Mic_tuning.direction <= 70):
-            flag = 'n';
-        if(70 < Mic_tuning.direction <= 75):
-            flag = 'o';
-        if(75 < Mic_tuning.direction <= 80):
-            flag = 'p';
-        if(80 < Mic_tuning.direction <= 85):
-            flag = 'q';
-        if(85 < Mic_tuning.direction <= 90):
-            flag = 'r';
-        if(90 < Mic_tuning.direction <= 95):
-            flag = 's';
-        if(95 < Mic_tuning.direction <= 100):
-            flag = 't';
-        if(100 < Mic_tuning.direction <= 105):
-            flag = 'u';
-        if(105 < Mic_tuning.direction <= 110):
-            flag = 'v';
-        if(110 < Mic_tuning.direction <= 115):
-            flag = 'w';
-        if(115 < Mic_tuning.direction <= 120):
-            flag = 'x';
-        if(120 < Mic_tuning.direction <= 125):
-            flag = 'y';
-        if(125 < Mic_tuning.direction <= 130):
-            flag = 'z';
-        if(130 < Mic_tuning.direction <= 135):
-            flag = '0';
-        if(135 < Mic_tuning.direction <= 140):
-            flag = '1';
-        if(140 < Mic_tuning.direction <= 145):
-            flag = '2';
-        if(145 < Mic_tuning.direction <= 150):
-            flag = '3';
-        if(150 < Mic_tuning.direction <= 155):
-            flag = '4';
-        if(155 < Mic_tuning.direction <= 160):
-            flag = '5';
-        if(160 < Mic_tuning.direction <= 165):
-            flag = '6';
-        if(165 < Mic_tuning.direction <= 170):
-            flag = '7';
-        if(170 < Mic_tuning.direction <= 175):
-            flag = '8';
-        if(175 < Mic_tuning.direction <= 180):
-            flag = '9';
-        print flag
-        ser.write(flag)
-        time.sleep(0.01)
-        if(flag == '@'):
-            break;
+        # if Mic_tuning.is_voice() == 1:
+            print Mic_tuning.direction
+            if(0 <= Mic_tuning.direction <= 5):
+                flag = 'a'
+            if(5 < Mic_tuning.direction <= 10):
+                flag = 'b';
+            if(10 < Mic_tuning.direction <= 15):
+                flag = 'c';
+            if(15 < Mic_tuning.direction <= 20):
+                flag = 'd';
+            if(20 < Mic_tuning.direction <= 25):
+                flag = 'e';
+            if(25 < Mic_tuning.direction <= 30):
+                flag = 'f';
+            if(30 < Mic_tuning.direction <= 35):
+                flag = 'g';
+            if(35 < Mic_tuning.direction <= 40):
+                flag = 'h';
+            if(40 < Mic_tuning.direction <= 45):
+                flag = 'i';
+            if(45 < Mic_tuning.direction <= 50):
+                flag = 'j';
+            if(50 < Mic_tuning.direction <= 55):
+                flag = 'k';
+            if(55 < Mic_tuning.direction <= 60):
+                flag = 'l';
+            if(60 < Mic_tuning.direction <= 65):
+                flag = 'm';
+            if(65 < Mic_tuning.direction <= 70):
+                flag = 'n';
+            if(70 < Mic_tuning.direction <= 75):
+                flag = 'o';
+            if(75 < Mic_tuning.direction <= 80):
+                flag = 'p';
+            if(80 < Mic_tuning.direction <= 85):
+                flag = 'q';
+            if(85 < Mic_tuning.direction <= 90):
+                flag = 'r';
+            if(90 < Mic_tuning.direction <= 95):
+                flag = 's';
+            if(95 < Mic_tuning.direction <= 100):
+                flag = 't';
+            if(100 < Mic_tuning.direction <= 105):
+                flag = 'u';
+            if(105 < Mic_tuning.direction <= 110):
+                flag = 'v';
+            if(110 < Mic_tuning.direction <= 115):
+                flag = 'w';
+            if(115 < Mic_tuning.direction <= 120):
+                flag = 'x';
+            if(120 < Mic_tuning.direction <= 125):
+                flag = 'y';
+            if(125 < Mic_tuning.direction <= 130):
+                flag = 'z';
+            if(130 < Mic_tuning.direction <= 135):
+                flag = '0';
+            if(135 < Mic_tuning.direction <= 140):
+                flag = '1';
+            if(140 < Mic_tuning.direction <= 145):
+                flag = '2';
+            if(145 < Mic_tuning.direction <= 150):
+                flag = '3';
+            if(150 < Mic_tuning.direction <= 155):
+                flag = '4';
+            if(155 < Mic_tuning.direction <= 160):
+                flag = '5';
+            if(160 < Mic_tuning.direction <= 165):
+                flag = '6';
+            if(165 < Mic_tuning.direction <= 170):
+                flag = '7';
+            if(170 < Mic_tuning.direction <= 175):
+                flag = '8';
+            if(175 < Mic_tuning.direction <= 180):
+                flag = '9';
+            if(180 < Mic_tuning.direction <= 270):
+                flag = flag;
+            print flag
+            ser.write(flag)
+            time.sleep(0.1)
+            if(flag == '@'):
+                break;
     ser.close()
